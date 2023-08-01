8de933b53642ce861aa56346c507e7e7da50dd90
Ali Shahidi <alishahidi1376@gmail.com>
2023-08-01 22:22:52 +0330

    update main file for using searches

--- src/main/java/org/alishahidi/Main.java
+++ src/main/java/org/alishahidi/Main.java
@@ -1,7 +1,15 @@
 package org.alishahidi;
 
+import org.alishahidi.search.BinarySearch;
+import org.alishahidi.search.InterpolationSearch;
+import org.alishahidi.search.LinearSearch;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] array = {1, 2, 3, 4, 5, 6, 11, 14, 17, 22, 33};
+
+        System.out.printf(String.valueOf(LinearSearch.search(array, 22)));
+        System.out.printf(String.valueOf(BinarySearch.search(array, 22)));
+        System.out.printf(String.valueOf(InterpolationSearch.search(array, 22)));
     }
 }
